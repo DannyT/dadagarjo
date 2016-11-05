8b3f3a4d39927434cbe8062bdf6cdba1cc2aba24
Gareth Williams <gareth@garethw.net>
2016-11-05 21:35:09 +0000

    Chest opens

---
+++ Unity/Assets/Scripts/Chest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour
+{
+    public bool IsOpen = false;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Player>() != null)
+        {
+            anim.Play("open", 0);
+            IsOpen = true;
+        }
+    }
+}
