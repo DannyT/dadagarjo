92b60ca04c1e0bcaacc62a465d3506bf20321db2
Gareth Williams <gareth@garethw.net>
2016-11-05 16:25:54 +0000

    Gareth Unity first dump

---
+++ Unity/Assets/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    float HP = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(HP<=0f)
+            Destroy(gameObject);
+	}
+
+    public void HitBySword()
+    {
+        HP--;
+
+    }
+}
---
+++ Unity/Assets/Sword.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Hit()
+    {
+        transform.GetComponentInParent<Player>().Attack();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Enemy e = col.transform.GetComponent<Enemy>();
+        if (e != null && GetComponent<Animation>().isPlaying)
+        {
+            e.HitBySword();
+        }
+    }
+}
